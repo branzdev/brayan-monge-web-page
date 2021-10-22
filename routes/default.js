64af7bbdc9c5d3e55a89d637f53078075b237601
Brayan Monge <bmongemendez@gmail.com>
2021-10-21 22:47:36 -0600

    Configured server to serve frontend and also configured automatic https and non www 301 redirection

--- routes/default.js
+++ routes/default.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET default page. */
+/* GET default page and check if it has www and https. */
 router.get('/', function (req, res, next) {
-	if (req.headers.host.slice(0, 3) === 'www') {
-		res.redirect(301, 'https://' + req.get('host').slice(3) + req.url);
-	} else if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-		res.redirect(301, 'https://' + req.get('host') + req.url);
+	if (process.env.NODE_ENV === 'production') {
+		if (req.headers.host.slice(0, 3) === 'www') {
+			res.redirect(301, 'https://' + req.get('host').slice(4) + req.url);
+		} else if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+			res.redirect(301, 'https://' + req.get('host') + req.url);
+		} else {
+			next();
+		}
 	} else {
 		next();
 	}
