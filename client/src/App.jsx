d5ff0e0dbd39e18f61c2543cf43fd748016f5e29
Brayan Monge <bmongemendez@gmail.com>
2023-08-17 23:32:31 -0600

    Feat: Update React Router to v6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //Components
-import Home from './components/Home/Home';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Header from './components/Header/Header';
-import SideNav from './components/SideNav/SideNav';
-import ParticlesBackground from './components/ParticlesBackground/ParticlesBackground';
+import Home from "./components/Home/Home";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import SideNav from "./components/SideNav/SideNav";
+import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
 
 function App() {
 	return (
 		<Router>
 			<ParticlesBackground />
 			<Header />
-			<Route exact path="/">
-				<Home />
-			</Route>
-			<Route path="/projects">
-				<Projects />
-			</Route>
-			<Route path="/contact">
-				<Contact />
-			</Route>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/projects" element={<Projects />}>
+					<Route path=":id" />
+				</Route>
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 			<SideNav />
 		</Router>
 	);
--- client/src/components/Projects/Projects.jsx
+++ client/src/components/Projects/Projects.jsx
@@ -1,83 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 //Styles
-import './projects.scss';
+import "./projects.scss";
 //Components
-import Project from '../Project/Project';
+import Project from "../Project/Project";
 //Logos
-import logoHTML5 from '../../img/HTML5.svg';
-import logoCSS3 from '../../img/CSS3.svg';
-import logoJS from '../../img/JS.svg';
-import logoMongoDB from '../../img/MongoDB.svg';
-import logoExpressJS from '../../img/ExpressJS.svg';
-import logoReactJS from '../../img/ReactJS.svg';
-import logoNodeJS from '../../img/NodeJS.svg';
-import logoSass from '../../img/Sass.svg';
-import logoUnity from '../../img/Unity.svg';
-import logoCSharp from '../../img/CSharp.svg';
+import logoHTML5 from "../../img/HTML5.svg";
+import logoCSS3 from "../../img/CSS3.svg";
+import logoJS from "../../img/JS.svg";
+import logoMongoDB from "../../img/MongoDB.svg";
+import logoExpressJS from "../../img/ExpressJS.svg";
+import logoReactJS from "../../img/ReactJS.svg";
+import logoNodeJS from "../../img/NodeJS.svg";
+import logoSass from "../../img/Sass.svg";
+import logoUnity from "../../img/Unity.svg";
+import logoCSharp from "../../img/CSharp.svg";
 
 export default function Proyects() {
 	// eslint-disable-next-line no-unused-vars
 	const [projects, setProjects] = useState([
 		{
-			title: 'JMéndez',
-			titleSpan: 'Constructora',
-			subTitle: 'Web Page',
+			title: "JMéndez",
+			titleSpan: "Constructora",
+			subTitle: "Web Page",
 			paragraph:
-				'Creation of a website capable of giving customers relevant information about the company and also the opportunity of a direct contact option',
+				"Creation of a website capable of giving customers relevant information about the company and also the opportunity of a direct contact option",
 			logos: [logoHTML5, logoSass, logoNodeJS, logoReactJS, logoExpressJS],
 		},
 		{
-			title: 'My',
-			titleSpan: 'Personal',
-			subTitle: 'Web Page',
-			utility: '(.this)',
+			title: "My",
+			titleSpan: "Personal",
+			subTitle: "Web Page",
+			utility: "(.this)",
 			paragraph:
-				'Creation of my personal website where all visitants can see my most recent projects, get a copy of my resume and contact me directly',
+				"Creation of my personal website where all visitants can see my most recent projects, get a copy of my resume and contact me directly",
 			logos: [logoHTML5, logoSass, logoNodeJS, logoReactJS, logoExpressJS],
 		},
 		{
-			title: 'Another Version',
-			titleSpan: 'of My Portfolio',
-			subTitle: 'Web Page',
+			title: "Another Version",
+			titleSpan: "of My Portfolio",
+			subTitle: "Web Page",
 			paragraph:
-				'Creation of my portfolio with another design and structure with plain HTML, CSS and JavaScript',
+				"Creation of my portfolio with another design and structure with plain HTML, CSS and JavaScript",
 			logos: [logoHTML5, logoCSS3, logoJS],
 		},
 		{
-			title: 'Turn ',
-			titleSpan: 'The Jump!',
-			subTitle: 'Videogame',
-			utility: '(WIP)',
+			title: "Turn ",
+			titleSpan: "The Jump!",
+			subTitle: "Videogame",
+			utility: "(WIP)",
 			paragraph: 'I am building my own 2D game inspired by "Doddle Jump"',
 			logos: [logoUnity, logoCSharp],
 		},
 		{
-			title: 'And more',
-			titleSpan: 'Projects',
-			subTitle: 'On the way!',
+			title: "And more",
+			titleSpan: "Projects",
+			subTitle: "On the way!",
 			paragraph: `I'm constantly learning and practicing new skills everyday :)`,
-			logos: [logoHTML5, logoCSS3, logoJS, logoMongoDB, logoExpressJS, logoReactJS, logoNodeJS],
+			logos: [
+				logoHTML5,
+				logoCSS3,
+				logoJS,
+				logoMongoDB,
+				logoExpressJS,
+				logoReactJS,
+				logoNodeJS,
+			],
 		},
 	]);
 	const [currentProject, setCurrentProject] = useState(0);
 	const [isGoingRight, setIsGoingRight] = useState(true);
 
-	let history = useHistory();
+	let navigate = useNavigate();
+
 	const location = useLocation();
 
 	useEffect(() => {
-		history.push('/projects/' + currentProject);
-	}, [currentProject, history]);
+		navigate("/projects/" + currentProject);
+	}, [currentProject, navigate]);
 
 	const handleLeftClick = () => {
 		currentProject >= 1 && setCurrentProject((prevState) => prevState - 1);
 		setIsGoingRight(false);
 	};
 	const handleRightClick = () => {
-		currentProject < projects.length - 1 && setCurrentProject((prevState) => prevState + 1);
+		currentProject < projects.length - 1 &&
+			setCurrentProject((prevState) => prevState + 1);
 		setIsGoingRight(true);
 	};
 
@@ -94,7 +104,12 @@ export default function Proyects() {
 					onClick={handleLeftClick}
 				>
 					<div className="projects__leftArrow">
-						<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="48"
+							height="48"
+							viewBox="0 0 48 48"
+						>
 							<title>Go Left</title>
 							<g fill="#ffffff">
 								<path d="M30.83 32.67l-9.17-9.17 9.17-9.17L28 11.5l-12 12 12 12z"></path>
@@ -103,9 +118,10 @@ export default function Proyects() {
 					</div>
 				</motion.div>
 				<AnimatePresence>
-					<Switch location={location} key={location.pathname}>
-						<Route exact path="/projects/0">
-							<div>
+					<Routes location={location} key={location.pathname}>
+						<Route
+							path="0"
+							element={
 								<Project
 									isGoingRight={isGoingRight}
 									title={projects[0].title}
@@ -117,73 +133,88 @@ export default function Proyects() {
 									github="https://github.com/bmongemendez/JM-Constructora"
 									button
 								/>
-							</div>
-						</Route>
-						<Route path="/projects/1">
-							<Project
-								isGoingRight={isGoingRight}
-								title={projects[1].title}
-								titleSpan={projects[1].titleSpan}
-								utility={projects[1].utility}
-								subTitle={projects[1].subTitle}
-								paragraph={projects[1].paragraph}
-								logos={projects[1].logos}
-								github="https://github.com/bmongemendez/brayan-monge-web-page"
-								button
-							/>
-						</Route>
-						<Route path="/projects/2">
-							<Project
-								isGoingRight={isGoingRight}
-								title={projects[2].title}
-								titleSpan={projects[2].titleSpan}
-								utility={projects[2].utility}
-								subTitle={projects[2].subTitle}
-								paragraph={projects[2].paragraph}
-								logos={projects[2].logos}
-								github="https://github.com/bmongemendez/personal-portfolio-webpage-fcc"
-								website="https://codepen.io/bmongemendez/full/abyOBeq"
-								button
-							/>
-						</Route>
-						<Route path="/projects/3">
-							<Project
-								isGoingRight={isGoingRight}
-								title={projects[3].title}
-								titleSpan={projects[3].titleSpan}
-								subTitle={projects[3].subTitle}
-								utility={projects[3].utility}
-								paragraph={projects[3].paragraph}
-								logos={projects[3].logos}
-								github="https://github.com/bmongemendez/TurnTheJump"
-								button
-							/>
-						</Route>
-						<Route path="/projects/4">
-							<Project
-								isGoingRight={isGoingRight}
-								title={projects[4].title}
-								titleSpan={projects[4].titleSpan}
-								subTitle={projects[4].subTitle}
-								paragraph={projects[4].paragraph}
-								logos={projects[4].logos}
-								button
-								github="https://github.com/bmongemendez"
-							/>
-						</Route>
-						<Route path="/projects/3">
-							<Project
-								isGoingRight={isGoingRight}
-								title={projects[3].title}
-								titleSpan={projects[3].titleSpan}
-								subTitle={projects[3].subTitle}
-								paragraph={projects[3].paragraph}
-								logos={projects[3].logos}
-								button
-								github="https://github.com/bmongemendez"
-							/>
-						</Route>
-					</Switch>
+							}
+						></Route>
+						<Route
+							path="1"
+							element={
+								<Project
+									isGoingRight={isGoingRight}
+									title={projects[1].title}
+									titleSpan={projects[1].titleSpan}
+									utility={projects[1].utility}
+									subTitle={projects[1].subTitle}
+									paragraph={projects[1].paragraph}
+									logos={projects[1].logos}
+									github="https://github.com/bmongemendez/brayan-monge-web-page"
+									button
+								/>
+							}
+						></Route>
+						<Route
+							path="2"
+							element={
+								<Project
+									isGoingRight={isGoingRight}
+									title={projects[2].title}
+									titleSpan={projects[2].titleSpan}
+									utility={projects[2].utility}
+									subTitle={projects[2].subTitle}
+									paragraph={projects[2].paragraph}
+									logos={projects[2].logos}
+									github="https://github.com/bmongemendez/personal-portfolio-webpage-fcc"
+									website="https://codepen.io/bmongemendez/full/abyOBeq"
+									button
+								/>
+							}
+						></Route>
+						<Route
+							path="3"
+							element={
+								<Project
+									isGoingRight={isGoingRight}
+									title={projects[3].title}
+									titleSpan={projects[3].titleSpan}
+									subTitle={projects[3].subTitle}
+									utility={projects[3].utility}
+									paragraph={projects[3].paragraph}
+									logos={projects[3].logos}
+									github="https://github.com/bmongemendez/TurnTheJump"
+									button
+								/>
+							}
+						></Route>
+						<Route
+							path="4"
+							element={
+								<Project
+									isGoingRight={isGoingRight}
+									title={projects[4].title}
+									titleSpan={projects[4].titleSpan}
+									subTitle={projects[4].subTitle}
+									paragraph={projects[4].paragraph}
+									logos={projects[4].logos}
+									button
+									github="https://github.com/bmongemendez"
+								/>
+							}
+						></Route>
+						<Route
+							path="3"
+							element={
+								<Project
+									isGoingRight={isGoingRight}
+									title={projects[3].title}
+									titleSpan={projects[3].titleSpan}
+									subTitle={projects[3].subTitle}
+									paragraph={projects[3].paragraph}
+									logos={projects[3].logos}
+									button
+									github="https://github.com/bmongemendez"
+								/>
+							}
+						></Route>
+					</Routes>
 				</AnimatePresence>
 				<motion.div
 					whileTap={{ scale: 0.9 }}
@@ -191,7 +222,12 @@ export default function Proyects() {
 					onClick={handleRightClick}
 				>
 					<div className="projects__rightArrow">
-						<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="48"
+							height="48"
+							viewBox="0 0 48 48"
+						>
 							<title>Go Right</title>
 							<g fill="#ffffff">
 								<path d="M17.17 32.92l9.17-9.17-9.17-9.17L20 11.75l12 12-12 12z"></path>
@@ -206,7 +242,7 @@ export default function Proyects() {
 							<motion.div
 								whileTap={{ scale: 0.9 }}
 								className={`projects__current-selection ${index} ${
-									index === currentProject && 'active'
+									index === currentProject && "active"
 								}`}
 								key={index}
 								onClick={() => handleCurrentProjectClick(index)}
